c9924b7d8dc281d26654929491a3c5e3430eb14b
Jorge Araya <jorge.araya@bvmedia.cr>
2022-05-12 21:36:11 -0600

    fix characters issue

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,23 @@
 import fs from "fs";
 import matter from "gray-matter";
-import HomePosts from "../components/homePosts";
+//import HomePosts from "../components/homePosts";
 
 export default function Home({ posts }) {
   return (
     <div className="content">
-      <div class="p-4 mb-5">
+      <div className="p-4 mb-5">
         <div className="grid grid-cols-1 md:grid-cols-2 p-4">
           <div className="py-8 info-txt order-last md:order-first">
             <h1 className="font-bold mb-3">
-              Hi 👋🏻, I'm Jorge Araya!
+              Hi 👋🏻, I&apos;m Jorge Araya!
             </h1>
             <p className="mb-2 text-xl text-justify">
-              I'm a full-stack developer living in Costa Rica, with a great passion for programming and design. 
+              I&apos;m a full-stack developer living in Costa Rica, with a great passion for programming and design. 
               With experience in many tools and technologies such as Python, JavsScript, React, PHP, 
               WordPress, and many more.
             </p>
             <p className="mb-2 text-xl text-justify">
-              I'm also a coffee lover, eager to take on new challenges and work on amazing projects.
+              I&apos;m also a coffee lover, eager to take on new challenges and work on amazing projects.
             </p>
           </div>
           <div className="p-2 flex items-center justify-center">
