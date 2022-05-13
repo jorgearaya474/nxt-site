53cfd72b239bfce2ca84dcda89a2edb9b5e3f6e5
Jorge Araya <jorge.araya@bvmedia.cr>
2022-05-13 13:00:58 -0600

    update home posts layout and style

--- components/homePosts.js
+++ components/homePosts.js
@@ -3,13 +3,10 @@ import Link from "next/link";
 
 export default function HomePosts(props) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+    <div id="posts-list" className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
       {props.posts.map(({ slug, frontmatter }) => (
-        <div
-          key={slug}
-          className="pr-2"
-        >
-            <div className="border border-gray-200 rounded-xl shadow-lg overflow-hidden flex flex-col">
+        <div key={slug} className="p-2 post-box">
+            <div className="bg-slate-800 rounded-xl shadow-lg overflow-hidden flex flex-col hover:bg-slate-700 transition duration-300 ease-in-out">
             <Link href={`/blog/${slug}`}>
             <a>
               <Image
@@ -18,8 +15,7 @@ export default function HomePosts(props) {
                 alt={frontmatter.title}
                 src={`/${frontmatter.socialImage}`}
               />
-
-              <h1 className="p-4">{frontmatter.title}</h1>
+              <h1 className="p-4 text-white">{frontmatter.title}</h1>
             </a>
           </Link>
             </div>
