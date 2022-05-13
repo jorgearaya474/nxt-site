675f85c9ae5563c38e728b618ab0f0ba1ae5151c
Jorge Araya <jorge.araya@bvmedia.cr>
2022-05-13 13:01:10 -0600

    add tab

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
   return (
     <div id="nxt_site" className="flex flex-col min-h-screen">
       <Header/>
-      <main className="container mx-auto flex-1">{children}</main>
+        <main className="container mx-auto flex-1">{children}</main>
       <Footer/>
     </div>
   );
