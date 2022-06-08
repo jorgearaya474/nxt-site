a556c9c8685e70eddc53a12e2ad08bdebee6ac8f
Jorge Araya <jorgearaya474@gmail.com>
2022-06-07 22:09:25 -0600

    get posts in post template

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import matter from 'gray-matter';
 import md from 'markdown-it';
+import SidebarArticles from '../../components/sidebarArticles';
 
 export async function getStaticPaths() {
     // Get all posts
@@ -21,18 +22,39 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { slug } }) {
     const fileName = fs.readFileSync(`posts/${slug}.mdx`, 'utf-8');
     const { data: frontmatter, content } = matter(fileName);
+
+    // get post list
+    const postsFiles = fs.readdirSync("posts");
+    const posts = postsFiles.map((fileName) => {
+        const slug = fileName.replace(".mdx", "");
+        const readFile = fs.readFileSync(`posts/${fileName}`, "utf-8");
+        const { data: frontmatter } = matter(readFile);
+        return {
+        slug,
+        frontmatter,
+        };
+    });
+
     return {
         props: {
             frontmatter,
-            content
+            content,
+            posts
         }
     }
 }
 
-export default function postpage({ frontmatter, content }) {
+export default function postpage({ frontmatter, content, posts }) {
     return (
-        <article className='post prose lg:prose-xl p-4'>
-            <div className='content' dangerouslySetInnerHTML={{ __html: md().render(content) }} />
-        </article>
+        <div class="grid grid-cols-3 gap-4">
+            <div className='col-span-2'>
+                <article className='post prose lg:prose-xl p-4'>
+                    <div className='content' dangerouslySetInnerHTML={{ __html: md().render(content) }} />
+                </article>
+            </div>
+            <div>
+                <SidebarArticles posts={posts}/>
+            </div>
+        </div>
     )
 }
