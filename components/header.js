2a52dd017e7e1cf43554d3a453cf8b96d3e61151
Jorge Araya <jorgearaya474@gmail.com>
2022-06-07 22:10:59 -0600

    update header to use menu

--- components/header.js
+++ components/header.js
@@ -1,25 +1,31 @@
-import Link from 'next/link'
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Header() {
-    return (
-        <header className="mb-8 py-4 bg-blue-500"> 
-            <div className="container mx-auto flex-1">
-                <nav className="flex sm:justify-left space-x-4 px-6">
-                    <div className="flex items-center flex-shrink-0 text-white mr-6">
-                        <Link href="/">
-                            <a className="font-bold">Jorge Araya</a>
-                        </Link>
-                    </div>
-                    {/*[
-                        ['Blog', '/blog'],
-                        ['Contact', '/contact'],
-                        ['Projects', '/projects'],
-                    ].map(([title, url]) => (
-                        <a key={title} href={url} className="rounded-lg px-3 py-2 text-white font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a>
-                    ))*/}
-                </nav>
-            </div>
-      </header>
-    );
+  const router = useRouter();
+  return (
+    <header className="mb-8 py-4">
+      <div className="container mx-auto flex-1">
+        <nav className="flex sm:justify-left space-x-4 px-4">
+          <div className="flex items-center flex-shrink-0 text-white mr-6">
+            {[
+              ["Home", "/"],
+              ["Blog", "/blog"],
+              ["Projects", "/projects"],
+            ].map(([title, url]) => (
+              <Link key={title} href={url}>
+                <a
+                  className={`rounded-lg px-3 py-2 font-bold text-lg text-white hover:bg-slate-100 hover:text-slate-900 ${
+                    router.pathname == url ? "text-emerald-500" : ""
+                  }`}
+                >
+                  {title}
+                </a>
+              </Link>
+            ))}
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
 }
-
