cc1e25ce36ca1ad52ecd856f4875ed27d7ae34f3
Jorge Araya <jorgearaya474@gmail.com>
2022-07-18 22:03:56 -0600

    add code mandala

--- pages/index.js
+++ pages/index.js
@@ -13,21 +13,96 @@ export default function Home({ posts }) {
                 Hi 👋🏻, I&apos;m Jorge Araya!
               </h1>
               <p className="mb-2 text-xl text-justify text-white">
-                I&apos;m a full-stack developer living in Costa Rica, with a great passion for programming and design. 
-                With experience in many tools and technologies such as Python, JavsScript, React, PHP, 
-                WordPress, and more.
+                I&apos;m a full-stack developer living in Costa Rica, with a
+                great passion for programming and design. With experience in
+                many tools and technologies such as Python, JavsScript, React,
+                PHP, WordPress, and more.
               </p>
               <p className="mb-2 text-xl text-justify text-white">
-                I&apos;m also a coffee lover, eager to take on new challenges and work on amazing projects.
+                I&apos;m also a coffee lover, eager to take on new challenges
+                and work on amazing projects.
               </p>
             </div>
             <div className="p-2 flex items-center justify-center">
-              <figure className="pf-picture">
+              <div className="pf-picture relative">
                 <img
-                  src="/images/profile-jorge-araya.png"
+                  src="/images/profile-02.png"
                   className="rounded-full mx-auto md:max-w-xs"
                 ></img>
-              </figure>
+
+                <div id="circle" className="absolute top-0">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    width="100%"
+                    height="100%"
+                    x="0"
+                    y="0"
+                    viewBox="0 0 300 300"
+                  >
+                    <defs>
+                      <path
+                        id="outer-circle"
+                        d="M15 150a135 135 0 01 270 0 135 135 0 01-270 0"
+                      >
+                        <animateTransform
+                          attributeName="transform"
+                          begin="0s"
+                          dur="400s"
+                          type="rotate"
+                          from="0 150 150"
+                          to="360 150 150"
+                          repeatCount="indefinite"
+                        ></animateTransform>
+                      </path>
+
+                      <path
+                        id="inner-circle"
+                        d="M40 150a110 110 0 01 220 0 110 110 0 01-220 0"
+                      >
+                        <animateTransform
+                          attributeName="transform"
+                          begin="0s"
+                          dur="400s"
+                          type="rotate"
+                          from="360 150 150"
+                          to="0 150 150"
+                          repeatCount="indefinite"
+                        ></animateTransform>
+                      </path>
+                    </defs>
+                    <circle cx="150" cy="150" r="150" fill="none"></circle>
+                    <use fill="none" xlinkHref="#outer-circle"></use>
+                    <text>
+                      <textPath xlinkHref="#outer-circle" fill="#fcd12878">
+                        <tspan className="tseparator"> &nbsp;&frasl;&frasl;&nbsp; </tspan>
+                        console.log(
+                        <tspan className="tstext">&quot;Hello World&quot;</tspan>);
+                        <tspan className="tseparator"> &nbsp;&frasl;&frasl;&nbsp; </tspan>
+                        echo <tspan className="tstext">&quot;Hello World&quot;</tspan>;
+                        <tspan className="tseparator"> &nbsp;&frasl;&frasl;&nbsp; </tspan>
+                        print(<tspan className="tstext">&quot;Hello World&quot;</tspan>)
+                        <tspan className="tseparator"> &nbsp;&frasl;&frasl;&nbsp; </tspan>
+                        System.out.println(
+                        <tspan className="tstext">&quot;Hello World&quot;</tspan>);
+                      </textPath>
+                    </text>
+                    <text>
+                      <textPath xlinkHref="#inner-circle" fill="#6d6d94">
+                        <tspan className="tseparator"> --- </tspan>
+                        background-color:{" "}
+                        <tspan className="tsval">#ffffff;</tspan>
+                        <tspan className="tseparator"> --- </tspan>
+                        width: <tspan className="tsval">100%;</tspan>
+                        <tspan className="tseparator"> --- </tspan>
+                        font-size: <tspan className="tsval">46px;</tspan>
+                        <tspan className="tseparator"> --- </tspan>
+                        padding: <tspan className="tsval">12px 14px;</tspan>
+                      </textPath>
+                    </text>
+                  </svg>
+                </div>
+              </div>
             </div>
           </div>
         </div>
