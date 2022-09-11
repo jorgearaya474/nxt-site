abc55bb4b3b9c4780b5a59bdf2d5eea3773a52ab
Jorge Araya <jorgearaya474@gmail.com>
2022-09-10 19:15:14 -0600

    update main layout component

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,18 @@
 import Header from "./header";
 import Footer from "./footer";
+import { useEffect } from "react";
+import { headerStickyAnimate, loadTitleMask  } from "./utils/utils";
 
 export default function Layout({ children }) {
+  useEffect(() => {
+    headerStickyAnimate();
+    loadTitleMask();
+  })
   return (
     <div id="nxt_site" className="flex flex-col min-h-screen">
-      <Header/>
-        <main className="container mx-auto flex-1">{children}</main>
-      <Footer/>
+        <Header/>
+        <main className="">{children}</main>
+        <Footer/>
     </div>
   );
 }
