7280969926bf18a0d85f902de0b120233acc9df8
Jorgearaya47 <jorgearaya474@gmail.com>
2023-08-16 22:26:42 -0600

    fix menu padding on ipad

--- components/layout/MainHeader.tsx
+++ components/layout/MainHeader.tsx
@@ -17,7 +17,7 @@ const MainHeader: React.FC = () => {
 
   return (
     <header id='site-header' className='fixed w-full top-0 z-50 transition-all duration-300 shadow-lg bg-white'>
-      <div className='container mx-auto flex-1 py-2 md:py-6'>
+      <div className='container mx-auto flex-1 py-2 lg:py-6'>
         <nav className='flex items-center justify-between flex-wrap'>
           <div className="w-full flex flex-wrap items-center justify-between mx-auto">
             <Link href='/' className='flex items-center'>
