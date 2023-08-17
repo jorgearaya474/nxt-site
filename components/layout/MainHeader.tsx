a9703b669c7370da6f6f982229e355e4bcb0115a
Jorgearaya47 <jorgearaya474@gmail.com>
2023-08-16 22:12:19 -0600

    update layout components - new header

--- components/layout/MainHeader.tsx
+++ components/layout/MainHeader.tsx
@@ -1,46 +1,57 @@
 import React from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { useState } from 'react';
 
 const MainHeader: React.FC = () => {
-  const router = useRouter();
 
-  const [hasScrolled, setHasScrolled] = useState<boolean>(false);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollTop = window.pageYOffset;
-      if (scrollTop > 0) {
-        setHasScrolled(true);
-      } else {
-        setHasScrolled(false);
-      }
-    };
+  const handleMenuToggle = () => {
+    setIsMenuOpen((prevState) => !prevState);
+  }
 
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
-    <header id='site-header' className={`sticky top-0 z-50 bg-white transition-all duration-300 ${hasScrolled ? 'shadow-lg' : ''}`}>
-      <div className='container mx-auto flex-1'>
+    <header id='site-header' className='fixed w-full top-0 z-50 transition-all duration-300 shadow-lg bg-white'>
+      <div className='container mx-auto flex-1 py-2 md:py-6'>
         <nav className='flex items-center justify-between flex-wrap'>
-          <ul className='w-full flex justify-between'>
-            {[
-              ['Home', '/'],
-              ['About', '/#section_about'],
-              ['Contact', '/#section_contact'],
-              ['Blog', '/blog']
-            ].map(([title, url]) => (
-              <Link key={title} href={url} className='md:inline-block py-2 font-bold text-lg text-black'>
-                {title}
-              </Link>
-            ))}
-          </ul>
+          <div className="w-full flex flex-wrap items-center justify-between mx-auto">
+            <Link href='/' className='flex items-center'>
+              <Image
+                src='/images/jorge-araya-logo-black.png'
+                alt='Jorge Araya Logo'
+                width={167}
+                height={20}
+                className=''
+                loading='lazy'
+              />
+            </Link>
+            <button type="button" className='inline-flex items-center p-2 w-10 h-10 justify-center text-md md:hidden text-black' onClick={handleMenuToggle}>
+              <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+              </svg>
+            </button>
+            <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`}
+              id='navbar-default'>
+              <ul className='font-medium flex flex-col p-4 md:p-0 mt-4 md:flex-row md:space-x-8 md:mt-0'>
+                {[
+                  ['Home', '/'],
+                  ['About', '/#section_about'],
+                  //['My Work', '/work'],
+                  ['Contact', '/#section_contact'],
+                  ['Blog', '/blog']
+                ].map(([title, url]) => (
+                  <Link key={title} href={url} className='md:inline-block py-4 lg:py-2 font-bold text-lg text-center text-black' onClick={closeMenu}>
+                    {title}
+                  </Link>
+                ))}
+              </ul>
+            </div>
+          </div>
         </nav>
       </div >
     </header >
