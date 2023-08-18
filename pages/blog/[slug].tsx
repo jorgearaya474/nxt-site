a4c2e51c829adf4bf1de365b23f32016df212d8e
Jorgearaya47 <jorgearaya474@gmail.com>
2023-08-17 21:11:28 -0600

    update blog pages

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,21 +1,40 @@
 import fs from "fs";
+import md from 'markdown-it';
 import matter from "gray-matter";
 import { NextPage } from "next";
-import { SinglePost } from "../../interfaces";
-import SinglePostView from "../../components/blog/SinglePostView";
+import Head from 'next/head';
+import MiniHero from '../../components/layout/MiniHero';
 import { GetStaticPathsResult, GetStaticPropsContext } from "next";
+import { SinglePost } from "../../interfaces";
+import { Calendar } from 'react-feather';
 
 const Postpage: NextPage<SinglePost> = (props) => {
-  const { slug, title, image, excerpt, date, content } = props;
+  const { title, excerpt, date, content } = props;
+
   return (
-    <SinglePostView
-      slug={slug}
-      title={title}
-      image={image}
-      excerpt={excerpt}
-      date={date}
-      content={content}
-    />
+    <div className='content'>
+      <Head>
+        <title>{title} | jorgearaya.dev</title>
+        <meta name='description' content={excerpt} />
+      </Head>
+      <MiniHero title={title} />
+      <div className='single-post flex justify-center items-center py-10'>
+        <div className='container'>
+          <article className='post prose'>
+            <div className='meta-data text-slate-400'>
+              <span className='mr-3'>
+                <Calendar className='inline-flex mb-1 mr-2' size='18' />
+                {date}
+              </span>
+            </div>
+            <div
+              className='content'
+              dangerouslySetInnerHTML={{ __html: md().render(content) }}
+            />
+          </article>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -4,17 +4,23 @@ import fs from 'fs';
 import matter from 'gray-matter';
 import PostsList from '../../components/blog/PostsList';
 import { Post, BlogPosts } from '../../interfaces';
+import MiniHero from '../../components/layout/MiniHero';
+import Contact from '../../components/home-sections/Contact';
 
 const Blog: NextPage<BlogPosts> = (props) => {
   return (
-    <div className='content flex justify-center items-center pt-20 lg:pt-28'>
+    <div className='content'>
       <Head>
         <title>Blog | jorgearaya.dev</title>
+        <meta name='description' content="Discover insights and expertise in web development on my blog. Join me in exploring the dynamic world of coding, design, and innovation. Elevate your web development journey with valuable tips and perspectives from an experienced web developer" />
       </Head>
-      <div className='container'>
-        <h1 className='text-black font-bold text-2xl mb-6'>Latest Articles</h1>
-        <PostsList posts={props.posts} />
+      <MiniHero title='Latest Articles' />
+      <div className='flex justify-center items-center py-10'>
+        <div className='container'>
+          <PostsList posts={props.posts} />
+        </div>
       </div>
+      <Contact />
     </div>
   );
 }
