5f6d70602f3d06e5ffc17bb950444e24650cc785
Jorgearaya47 <jorgearaya474@gmail.com>
2023-08-17 21:11:09 -0600

    add projects interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -13,3 +13,16 @@ export interface BlogPosts {
 export interface SinglePost extends Post {
     content: any
 }
+
+export interface Project {
+    slug: string,
+    title: string,
+    image: string,
+    link: string,
+    technologies: string,
+    description: string
+}
+
+export interface Projects {
+    projects: Project[]
+}
