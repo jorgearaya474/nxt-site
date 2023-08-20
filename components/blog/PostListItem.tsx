77b5b1adfbad573f0de04b24e14d7752e05dbe96
Jorgearaya47 <jorgearaya474@gmail.com>
2023-08-19 22:29:43 -0600

    adjust trim

--- components/blog/PostListItem.tsx
+++ components/blog/PostListItem.tsx
@@ -24,7 +24,7 @@ const PostListItem: React.FC<Post> = ({ slug, title, image, excerpt }) => {
               {title}
             </h2>
           </Link>
-          <p className='text-black'>{`${trimWords(excerpt, 25)}...`}</p>
+          <p className='text-black'>{`${trimWords(excerpt, 22)}...`}</p>
           <Link href={`/blog/${slug}`} className='block w-fit mt-4 px-3 py-2 font-bold text-white bg-black hover:bg-orange-500 transition ease-in-out duration-150' title={title}>Read More</Link>
         </div>
       </div>
