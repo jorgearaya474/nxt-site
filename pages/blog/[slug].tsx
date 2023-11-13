77b94c2bf12f8ef8cb39bbf9c76e307a68b14387
Jorgearaya47 <jorgearaya474@gmail.com>
2023-11-12 22:45:22 -0600

    update meta description and add keywords

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -2,28 +2,36 @@ import fs from 'fs';
 import md from 'markdown-it';
 import matter from 'gray-matter';
 import { NextPage } from 'next';
+import Image from 'next/image';
 import { motion } from 'framer-motion';
 import Head from 'next/head';
 import MiniHero from '../../components/layout/MiniHero';
 import { GetStaticPathsResult, GetStaticPropsContext } from 'next';
-import { SinglePost } from '../../interfaces';
+import { SinglePost } from '../../types';
 import { Calendar } from 'react-feather';
 import {
   FadeUpVariantsContainer,
   FadeUpVariantsItem,
 } from '../../lib/utils/animations';
 
+const pathName = 'content/posts';
+
 const Postpage: NextPage<SinglePost> = (props) => {
-  const { title, excerpt, date, content } = props;
+  const { title, excerpt, date, content, image } = props;
+  const imageUrl = `/images/posts/${image}`;
 
   return (
     <div className='content'>
       <Head>
         <title>{title} | jorgearaya.dev</title>
         <meta name='description' content={excerpt} />
+        <meta
+          name='keywords'
+          content='web development, website development, WordPress development, Costa Rica web design, React development, jorge araya, wordpress blog'
+        />
       </Head>
       <MiniHero title={title} />
-      <div className='single-post flex justify-center items-center py-10'>
+      <div className='single-post flex justify-center items-center py-4'>
         <motion.div
           className='container'
           variants={FadeUpVariantsContainer}
@@ -31,6 +39,9 @@ const Postpage: NextPage<SinglePost> = (props) => {
           animate='visible'
         >
           <article className='post prose'>
+            <div>
+              <Image width={1000} height={600} src={imageUrl} alt={title} />
+            </div>
             <div className='meta-data text-slate-400'>
               <span className='mr-3'>
                 <Calendar className='inline-flex mb-1 mr-2' size='18' />
@@ -51,7 +62,7 @@ const Postpage: NextPage<SinglePost> = (props) => {
 export default Postpage;
 
 export async function getStaticPaths(): Promise<GetStaticPathsResult> {
-  const files: string[] = fs.readdirSync('posts');
+  const files: string[] = fs.readdirSync(pathName);
   const paths = files.map((fileName) => ({
     params: {
       slug: fileName.replace('.mdx', ''),
@@ -68,7 +79,7 @@ export async function getStaticProps({
   params,
 }: GetStaticPropsContext<{ slug: string }>): Promise<{ props: SinglePost }> {
   const { slug } = params!;
-  const filePath = `${process.cwd()}/posts/${slug}.mdx`;
+  const filePath = `${process.cwd()}/${pathName}/${slug}.mdx`;
   const fileContent = fs.readFileSync(filePath, 'utf-8');
   const { data: frontmatter, content } = matter(fileContent);
   return {
@@ -78,6 +89,7 @@ export async function getStaticProps({
       image: frontmatter.image as string,
       excerpt: frontmatter.excerpt as string,
       date: frontmatter.date as string,
+      isFeatured: frontmatter.isFeatured as boolean,
       content,
     },
   };
--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,18 +1,27 @@
 import { NextPage } from 'next';
-import Head from 'next/head'
+import Head from 'next/head';
 import fs from 'fs';
 import matter from 'gray-matter';
 import PostsList from '../../components/blog/PostsList';
-import { Post, BlogPosts } from '../../interfaces';
+import { Post, BlogPosts } from '../../types';
 import MiniHero from '../../components/layout/MiniHero';
 import Contact from '../../components/home-sections/Contact';
+
+const pathName = 'content/posts';
 
 const Blog: NextPage<BlogPosts> = (props) => {
   return (
     <div className='content'>
       <Head>
         <title>Blog | jorgearaya.dev</title>
-        <meta name='description' content="Discover insights and expertise in web development on my blog. Join me in exploring the dynamic world of coding, design, and innovation. Elevate your web development journey with valuable tips and perspectives from an experienced web developer" />
+        <meta
+          name='description'
+          content='Discover insights and expertise in web development on my blog. Join me in exploring the dynamic world of coding, design, and innovation. Elevate your web development journey with valuable tips and perspectives from an experienced web developer'
+        />
+        <meta
+          name='keywords'
+          content='web developer, website development, WordPress development, Costa Rica web design, React development, Jorge Araya blog, responsive design, user experience, front-end development, back-end development, SEO optimization, jorge araya, wordpress blog'
+        />
       </Head>
       <MiniHero title='Latest Articles' />
       <div className='flex justify-center items-center py-10'>
@@ -23,16 +32,16 @@ const Blog: NextPage<BlogPosts> = (props) => {
       <Contact />
     </div>
   );
-}
+};
 
 export default Blog;
 
 export async function getStaticProps() {
-  const files = fs.readdirSync('posts');
+  const files = fs.readdirSync(pathName);
 
   const posts: Post[] = files.map((fileName) => {
     const slug = fileName.replace('.mdx', '');
-    const readFile = fs.readFileSync(`posts/${fileName}`, 'utf-8');
+    const readFile = fs.readFileSync(`${pathName}/${fileName}`, 'utf-8');
     const { data: frontmatter } = matter(readFile);
     return {
       slug,
@@ -40,6 +49,7 @@ export async function getStaticProps() {
       image: frontmatter.image as string,
       excerpt: frontmatter.excerpt as string,
       date: frontmatter.date as string,
+      isFeatured: frontmatter.isFeatured as boolean,
     };
   });
 
