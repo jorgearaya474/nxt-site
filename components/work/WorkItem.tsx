0103e2b3f763ee691013dea1f9d7a9083788ad9b
Jorgearaya47 <jorgearaya474@gmail.com>
2024-05-22 22:01:14 -0600

    update work components - remove description

--- components/work/WorkItem.tsx
+++ components/work/WorkItem.tsx
@@ -6,39 +6,33 @@ import { Link2 } from 'react-feather';
 import { Project } from '../../interfaces';
 import { FadeUpVariantsItem } from '../../lib/utils/animations';
 
-const WorkItem: React.FC<Project> = ({
-  title,
-  image,
-  link,
-  technologies,
-  description,
-}) => {
+const WorkItem: React.FC<Project> = ({ title, image, link, technologies }) => {
   const techArray = technologies.split(',');
 
   return (
     <motion.div
-      className='bg-white h-full content-start grid grid-cols-1 shadow-lg mb-8'
+      className='bg-white content-start grid grid-cols-1 shadow-xl'
       variants={FadeUpVariantsItem}
     >
-      <div>
+      <Link
+        href={link}
+        title={title}
+        target='_blank'
+        className='relative group'
+      >
         <Image width={650} height={520} alt={title} src={`/${image}`} />
-      </div>
-      <div className='p-6'>
-        <h2 className='text-black text-xl font-bold mb-2'>{title}</h2>
-        <p className='text-black mb-4'>{description}</p>
-        <Link href={link} className='text-amber-500' target='_blank'>
-          Visit web site <Link2 className='inline-flex' />
-        </Link>
-        <ul className='mt-2 flex flex-wrap' aria-label='Technologies used:'>
-          {techArray.map((tech) => (
-            <li key={tech} className='mr-1.5 mt-2'>
-              <div className='flex items-center rounded-full bg-blue-400/20 px-3 py-1 text-xs font-medium leading-5 text-blue-400 '>
-                {tech}
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
+        <div className='opacity-0 group-hover:opacity-100 transition-opacity p-6 absolute bottom-0 w-full bg-gradient-to-t from-black to-transparent z-10'>
+          <ul className='mt-2 flex flex-wrap' aria-label='Technologies used:'>
+            {techArray.map((tech) => (
+              <li key={tech} className='mr-1.5 mt-2'>
+                <div className='flex items-center rounded-full px-3 py-1 text-xs font-medium leading-5 text-blue-950 bg-amber-500 '>
+                  {tech}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Link>
     </motion.div>
   );
 };
